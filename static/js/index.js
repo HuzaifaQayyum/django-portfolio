158a8d9465aa798ce1a72e2355db7e76e5bcd84b
HuzaifaQayyum <huzaifaacf@gmail.com>
2021-09-10 09:38:14 -0700

    optimization added

--- static/js/index.js
+++ static/js/index.js
@@ -5,15 +5,23 @@
  if (typed) {
    let typed_strings = typed.getAttribute('data-typed-items')
    typed_strings = typed_strings.split(',')
-   new Typed('.typed', {
+   const typed_instance = new Typed('.typed', {
      strings: typed_strings,
      loop: true,
      typeSpeed: 150,
      backSpeed: 70,
      backDelay: 1500
    });
+
+   new IntersectionObserver((entries, observer) => 
+     entries.forEach(entry => entry.isIntersecting ? typed_instance.start() : typed_instance.stop()), {
+       rootMargin: '10px'
+     }).observe(typed)
  }
  
+
+
+
  /**
   * Skills animation
   */
@@ -34,32 +42,32 @@
  /**
   * Porfolio isotope and filter
   */
- window.addEventListener('load', () => {
-   let portfolioContainer = select('.portfolio-container');
-   if (portfolioContainer) {
-     let portfolioIsotope = new Isotope(portfolioContainer, {
-       itemSelector: '.portfolio-item'
-     });
+//  window.addEventListener('load', () => {
+//    let portfolioContainer = select('.portfolio-container');
+//    if (portfolioContainer) {
+//      let portfolioIsotope = new Isotope(portfolioContainer, {
+//        itemSelector: '.portfolio-item'
+//      });
  
-     let portfolioFilters = select('#portfolio-flters li', true);
+//      let portfolioFilters = select('#portfolio-flters li', true);
  
-     on('click', '#portfolio-flters li', function (e) {
-       e.preventDefault();
-       portfolioFilters.forEach(function (el) {
-         el.classList.remove('filter-active');
-       });
-       this.classList.add('filter-active');
+//      on('click', '#portfolio-flters li', function (e) {
+//        e.preventDefault();
+//        portfolioFilters.forEach(function (el) {
+//          el.classList.remove('filter-active');
+//        });
+//        this.classList.add('filter-active');
  
-       portfolioIsotope.arrange({
-         filter: this.getAttribute('data-filter')
-       });
-       portfolioIsotope.on('arrangeComplete', function () {
-         AOS.refresh()
-       });
-     }, true);
-   }
+//        portfolioIsotope.arrange({
+//          filter: this.getAttribute('data-filter')
+//        });
+//        portfolioIsotope.on('arrangeComplete', function () {
+//          AOS.refresh()
+//        });
+//      }, true);
+//    }
  
- });
+//  });
  
  /**
   * Initiate portfolio lightbox 
@@ -77,50 +85,46 @@
    height: '90vh'
  });
  
- /**
-  * Portfolio details slider
-  */
- new Swiper('.portfolio-details-slider', {
-   speed: 400,
-   loop: true,
-   autoplay: {
-     delay: 5000,
-     disableOnInteraction: false
-   },
-   pagination: {
-     el: '.swiper-pagination',
-     type: 'bullets',
-     clickable: true
-   }
- });
  
  /**
   * Testimonials slider
   */
- new Swiper('.testimonials-slider', {
-   speed: 600,
-   loop: true,
-   autoplay: {
-     delay: 5000,
-     disableOnInteraction: false
-   },
-   slidesPerView: 'auto',
-   pagination: {
-     el: '.swiper-pagination',
-     type: 'bullets',
-     clickable: true
-   }
- });
+
+ const testimonialSlider = document.querySelector('.testimonials-slider');
+ if (testimonialSlider) {
+  const swiper = new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    spaceBetween: 100,
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry =>
+      entry.isIntersecting ? (swiper.autoplay.start() && swiper.attachEvents()) : 
+                             (swiper.autoplay.stop() && swiper.detachEvents())
+    )
+  }, { rootMargin: '10px' }).observe(testimonialSlider)
+}
 
 
  /**
  * Animation on scroll
  */
-window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-out',
-      mirror: false,
-      once: true
-    })
-  });
+// window.addEventListener('load', () => {
+//     AOS.init({
+//       duration: 1000,
+//       easing: 'ease-out',
+//       mirror: false,
+//       once: true
+//     })
+//   });
