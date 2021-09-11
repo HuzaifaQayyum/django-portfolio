d68b1c62152389764e5b7285da31ebfd2409626d
HuzaifaQayyum <huzaifaacf@gmail.com>
2021-09-11 11:04:42 -0700

    image not found Error fixed

--- static/vendor/fluid-simulation/webgl_fluid_simulation.js
+++ static/vendor/fluid-simulation/webgl_fluid_simulation.js
@@ -808,7 +808,7 @@ let bloomFramebuffers = [];
 let sunrays;
 let sunraysTemp;
 
-let ditheringTexture = createTextureAsync('LDR_LLL1_0.png');
+let ditheringTexture = createTextureAsync('/static/img/LDR_LLL1_0.png');
 
 const blurProgram            = new Program(blurVertexShader, blurShader);
 const copyProgram            = new Program(baseVertexShader, copyShader);
